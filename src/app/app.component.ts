86fb646cb1ee5cd02f5be6cc6e49e5dc9c8797e9
cryptohgn <cryptohgn@gmail.com>
2024-05-21 20:46:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularDirectives';
+  //ngIf & ngFor
+  show: boolean = false;
+  listItems = [
+    {
+      name: 'Alex',
+      age: 23,
+    },
+    {
+      name: 'Jack',
+      age: 25,
+    },
+    {
+      name: 'Ryan',
+      age: 27,
+    },
+    {
+      name: 'Shawn',
+      age: 24,
+    },
+    {
+      name: 'Bella',
+      age: 29,
+    },
+    {
+      name: 'Peter',
+      age: 32,
+    },
+  ];
+
+
+  //ngFor
+ 
+  // fullStackDev = [{
+  //   id : 1,
+  //   name : 'Angular'
+  //  },
+  //  {
+  //   id : 2,
+  //   name : 'React'
+  //  },
+  //  {
+  //   id : 3,
+  //   name : 'Node.js'
+  //  },
+  //  {
+  //   id : 4,
+  //   name : 'MongoDB'
+  //  }];
 }
